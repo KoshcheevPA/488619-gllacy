49d211ca53f1c0e54681cd83a94d8f0c415f1636
KoshcheevPA <42152174+KoshcheevPA@users.noreply.github.com>
2018-09-07 12:47:42 +0500

    Js
    Добавлен overlay. Реализовано скрытие popup'а по клику на оверлэй или крестик

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 var link = document.querySelector('.feedback-link');
 var popup = document.querySelector('.feedback-window');
 var close = document.querySelector('.feedback-close');
+var overlay = document.querySelector('.overlay')
 
 link.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.add('feedback-window-show');
+  overlay.classList.add('overlay-show');
 });
 
 close.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.remove('feedback-window-show');
+  overlay.classList.remove('overlay-show');
+});
+
+overlay.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.remove('feedback-window-show');
+  overlay.classList.remove('overlay-show');
 });
